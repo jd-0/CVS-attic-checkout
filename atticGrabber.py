3e93f81e5d3fdadc9a886c015d9d4bc50f669fdc
jd-0 <44876081+jd-0@users.noreply.github.com>
2020-09-06 09:33:51 -0700

    v2.0
    Change log:
    -Now seems to properly grab all files, sub-versions/branches
    -Skips writing empty files
    -More detailed logs, including commit messages and times for files/versions
    -Additional code commenting
    -Various other fixes, improvements, etc
    
    TODO:
    -Multiprocessing support?
    -Optimize code / improve speed!!
    -Ensure all files get extracted OK?
    -Check OS compatibility

--- atticGrabber.py
+++ atticGrabber.py
@@ -1,21 +1,21 @@
-##########################################
-# Description:
+#!/usr/bin/env python3
+# DESCRIPTION #############################
 # A basic, badly coded script to restore deleted Attic files from certain CVS repos
 # by removing the heading and tailing CVS info from files, and extracting the original data.
 
-# Requirements: Python 3.9
+# Requirements: Python 3.9 (for logging.basicConfig encoding)
 
-# Usage:
-# Place script in folder containing the repo, and run it.
-# Grab every version of every file:     ./atticGrabber.py -a True
-#                                       ./atticGrabber.py --all True
-# Grab ONLY v1.2 files:                 ./atticGrabber.py -v 1.2
-# Do a full dry run, with logging:      ./atticGrabber.py -l True -t True -a True
-# Grab ONLY most recent files:          ./atticGrabber.py -r True
+# USAGE #############################
+# Place script within a folder containing the repo folder, and run it.
 
-# Todo: 
-# DeDupe / detect files when grabbing all? 
-# Cleanup!!
+# Grab ONLY v1.2 files:                 python ./atticGrabber.py -v 1.2
+# Do a full dry run, with logging:      python ./atticGrabber.py -l True -t True
+# Grab ONLY most recent files:          python ./atticGrabber.py -r True
+
+########## TODO ###########
+# - Code optimizations?
+# - Multiprocessing? Speed improvements?
+###########################
 
 # IMPORTS #########################################
 import re
@@ -23,82 +23,87 @@ import os
 import mimetypes
 import logging
 import argparse
+from datetime import datetime
 
 # ARGUMENTS #########################################
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--version", default="1.1",
-                help="Specify a single file version number to grab")
-ap.add_argument("-a", "--allfiles", default=False, type=bool,
-                help="Grab all possible file versions")
-ap.add_argument("-l", "--logger", default=False, type=bool,
-                help="Output results to log.txt")
-ap.add_argument("-t", "--testrun", default=False, type=bool,
-                help="Do a dry run for testing & logging purposes")
-ap.add_argument("-r", "--recent", default=False, type=bool,
-                help="Grab ONLY most recent")
-args = vars(ap.parse_args())
-#verNo = int((str(args["version"]).split("."))[1])
-print("---atticGrabber v1.0---\n\nChecking out attics...\n")
-print("Settings:", args)
+ap.add_argument("-v", "--version", default="None", type=str, help="Specify a single file version number to grab")
+ap.add_argument("-l", "--logger", default=True, type=bool, help="Output results to log.txt")
+ap.add_argument("-t", "--testrun", default=False, type=bool, help="Do a dry run for testing & logging purposes")
+ap.add_argument("-r", "--recent", default=False, type=bool, help="Grab ONLY most recent")
+argsCMD = vars(ap.parse_args())
 
-# VARS #########################################
-encodings = ["ISO-8859-1","UTF-8"]
+# VARIABLES / INIT #########################################
+encodings = ["ISO-8859-1", "UTF-8", "shift-jis"]
 extensionsBin = ["bin", "sbin", "bnr", "bpl", "fnt", "gfx", "bnk", "bit", "fnt", "iff", "aifc", "raw", "rel", "swd", "szp", "szs", "sch", "seg", "zzz", "out", "cmd", "app", "res", "narc", "resdat", "cldat", "nsbmd", "ncl", "ncg", "nce", "nsc", "nftr", "pcx", "p3d"]
 extensionsText = ['Makedepend', 'Makefile', '.cvsignore', '.s', '.a', '.ev', '.evc', '.i', '.lsf', '.pch', '.imd', '.dfm', '.rsf', '.dtd', '.xlor', '.gmt', '.ose', '.bpr', '.mac', '.gly','.wid', '.nsbtx', '.mk', '.map', '.doc', '.tbl', '.bat', '.dat', '.gmm', '.naix', '.tmp', '.stdout', '.codebook', '.1st', '.lst']
-path = os.path.dirname(__file__) ; listOfFiles = list()
-for (dirpath, dirnames, filenames) in os.walk(path):
-    listOfFiles += [os.path.join(dirpath, file) for file in filenames]
-if args["logger"] == True: 
-    logging.basicConfig(filename=path+"\log.txt", filemode='a', level=logging.INFO, encoding=encodings[1])
+scriptPath = os.path.dirname(__file__) # Get file path based on script location
+def scanRecurse(baseDir): # Recursively List files to process
+    for entry in os.scandir(baseDir):
+        if entry.is_file(): yield entry.path
+        else: yield from scanRecurse(entry.path)
 
-# FUNCS #########################################
-def setPattern(x): 
-    verStr = bytes(str(x), encoding=encodings[1])
-    patternVer = re.compile(verStr + b'\nlog\n@(.*?)@\ntext\n@(.*?)@\n\n', re.DOTALL)
-    return patternVer
-patternVer = setPattern(args["version"])
+print("\n---atticGrabber v2.0---") # Display console info
+if argsCMD["logger"] == True: # Logging toggle check
+    logname = "{}/log_{}.txt".format(scriptPath, datetime.now().strftime("%d-%m-%y_%H-%M-%S"))
+    print("\nCreated logfile at: ", logname)
+    logging.basicConfig(filename=logname, filemode='a', level=logging.INFO, encoding=encodings[1])
+print("\nSettings:", argsCMD, "\n\nChecking out attics...\n")
 
-def writeFile(ver): # open the file as binary data and attempt a regex search
-        with open(currFile, "rb") as file: 
-                data = bytearray(file.read()) ; new_data = re.search(patternVer, data)
-        loggerText = ('\nGetting version: {}. Most recent: {}\nFileType: {}, Path:{}'.format(ver, fileCurrVer, currMime, currFile))
-        if new_data == None: return
-        else: new_data = (new_data.group(2)).replace(b'@@', b'@') # isolate new data, fix the double @@'s
-        if currMime == "text": new_data = new_data[:-1] # fix for text files and trailing @ character
+# FUNCTIONS #########################################
+def setPattern(ver): # set new regex pattern according to version
+    verStr = bytes(str(ver), encoding=encodings[1]).replace(b'.', b'\.') # Format the version string
+    pattern = re.compile(b'\n' + verStr + b'\nlog\n@(.*?)@\ntext\n@(.*?)@\n\n', re.DOTALL)
+    return pattern
 
-        # making sure dirs exist
-        newFile = currFile[:len(path) +1] +"v"+ str(ver) + currFile[len(path):]
-        if args["testrun"] == False: # Write the data to disk
-            os.makedirs(os.path.dirname(newFile), exist_ok=True)    
-            with open(newFile, "wb") as file: file.write(new_data)
-        if args["logger"] == True and args["testrun"] == False: loggerText += "\nWrote file.\n"
-        logging.info(loggerText)
+def updateLog(*args):
+    if argsCMD["logger"] == True: # Clear previous log data
+        loggerText = "" # Then, grab all input args, log to file
+        for info in args: loggerText += info ; logging.info(loggerText)
+        #print(loggerText)
+        
+def writeFile(currVer, currDate): 
+    patternNew = setPattern(currVer) # Set new pattern based on current version
+    newData = re.search(patternNew, currData) # extract data with regex
+    if newData == None or len(newData.group(2)) == 0: return # No op if empty data
+    try: commitMsg = str(newData.group(1), encodings[2]) # Get commit message
+    except: commitMsg = "" 
+    newData = (newData.group(2)).replace(b'@@', b'@') # Fix for nontext files
+
+    newFile = scriptPath + "/checkout/v" + currVer + "/" + currFile[len(scriptPath)+1:].split("\\", 1)[1] # Make a new filename
+    if argsCMD["testrun"] == False: # test/dry run enabled = skip writing files
+        os.makedirs(os.path.dirname(newFile), exist_ok=True) # make sure directories exist
+        with open(newFile, "wb") as file: file.write(newData) # write bytes to disk
+
+    # Finally, Write results to log
+    updateLog("\nWrote file: {}\nSize: {} bytes, Version: {}, Date: {}\nCommit message: {}\n".format(newFile, len(newData), currVer, currDate, commitMsg))
 
 # MAIN PROGRAM #########################################
-for currFile in listOfFiles:
-        # Get list of all ver no. in current file
-        with open(currFile, "r", encoding=encodings[0]) as file: 
-            fileCurrVer = re.sub("[;\n\t]", "", file.readline().strip("head")).split('.')
-            dataTemp = file.read()
-        if fileCurrVer[0] != '1': print("\nNothing to do for", currFile) ; continue
-        fileCurrVer = ".".join(fileCurrVer)
-        verList = [fileCurrVer]
-        verList.extend(re.findall("next	([0-9].*);",dataTemp))
-        #print(verList)
-        try: # try determine filetype 
-            if currFile.endswith(",v"): currFile[:-2]
-            currMime = str((mimetypes.guess_type(currFile))[0])[:4] 
-            if currMime == "None": # help out the mime with filetypes
-                for ext in extensionsText:
-                        if currFile.endswith(ext): currMime = 'text'
-                for ext2 in extensionsBin:
-                        if currFile.endswith(ext2): currMime = ext2       
-            if currFile.endswith('bat'): currMime = 'text'
-            if currFile == os.path.realpath(__file__): print("\nSkipping file", currFile) ; continue
-        except: print("Filetype error.") ; currMime = "None"
+startTime = datetime.now() ; updateLog("\nStart time: {}\n".format((startTime))) # Get start time
+listOfFiles = scanRecurse(scriptPath)
 
-        if args["allfiles"] == True: # CHECK ARGS
-            for x in verList: patternVer = setPattern(x) ; writeFile(x)
-        elif args["recent"] == True:
-            writeFile(fileCurrVer)
-        else: writeFile(args["version"])
+for currFile in listOfFiles: # Iterate thru listed files
+        if os.path.dirname(currFile) == scriptPath: continue
+        with open(currFile, "rb") as file: # Open file as bytes
+            currFileVer = file.readline() # Check if current file has CVS header info
+            if currFileVer.startswith(b"head") == False: updateLog("\nSkipping file:" + currFile + "\nNo CVS header found.\n") ; continue
+            currData = file.read() # Get file data if CVS info found
+        file_name, file_extension = os.path.splitext(currFile) # Get current file extension
+        currFileType = str(mimetypes.guess_type(currFile)[0])[:4] # Guess current fileType
+        if currFile.endswith('.bat'): currFileType = "text" # Treat .bat as text, not an app
+        allVersions = re.findall(b"(.*)\ndate\t(..........)",currData) # List all ver no. / dates in file Data
+        verList, dateList = map(list,zip(*allVersions)) # separate tuples, unpacks lists, byte decode, etc.
+        verList, dateList = [x.decode() for x in verList], [y.decode() for y in dateList]
+
+        # Write results to log
+        updateLog("\nReading file: {}\nAvailable versions: \n{}\n".format(currFile, str(verList)))
+
+        # CHECK CMD LINE ARGS, write data
+        if argsCMD["version"] != "None": writeFile(argsCMD["version"], "") # Gets specified version
+        elif argsCMD["recent"] == True: writeFile(verList[0], dateList[0]) # Gets "most recent" version
+        else: # If nothing specified, default action will be "grab everything"
+            for x in range(len(verList)): writeFile(verList[x], dateList[x])
+
+endTime = datetime.now()
+updateLog("\nTime taken (seconds): {}\nFinished at: {}\n".format(str(-(startTime - endTime).total_seconds()), endTime))
+print("\n...All done!")
