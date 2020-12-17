c1952d0e3f6bcbd2fb5629dbcbfb47769fe33ba0
jd-0 <44876081+jd-0@users.noreply.github.com>
2020-12-17 14:07:25 -0800

    Some fixes and add option to specify file encoding
    Fixed scriptPath to ow work on Linux
    Takes input argument for encoding to use, otherwise defaults to UTF-8

--- bulkCVScheckout.py
+++ bulkCVScheckout.py
@@ -3,24 +3,31 @@
 # A basic, badly coded script to checkout files from CVS repos
 
 # USAGE #############################
-# Place script within a folder containing the repo folder, and run it.
+# Place script within a folder containing the repo folder, then run it.
+# Optionally, specify an encoding if needed.
+# Example: 
+# python bulkCVScheckout.py
+# python bulkCVScheckout.py "shift-JIS"
 
 import re, os, sys
 
-scriptPath = (os.path.dirname(__file__)).replace("\\", "/") # Get file path based on script location
-
+scriptPath = (os.path.dirname(os.path.abspath(__file__))).replace("\\", "/") # Get file path based on script location
+encodings = ["None", "UTF-8", "shift-JIS", "ISO-8859-1", "Windows-1254", "ascii"]
+print(os.path.dirname(os.path.abspath(__file__)))
 def scanRecurse(baseDir): # Recursively Lists files to process
     for entry in os.scandir(baseDir):
         if entry.is_file(): yield entry.path
         else: yield from scanRecurse(entry.path)
         
-def main():
+def main(encoding):
     fileList = scanRecurse(scriptPath) # Find files to process
     for currFile in fileList:
         versionList,currFile = [],currFile.replace("\\", "/") # make path unix-styled
-        with open(currFile,'rb') as vFile: currData = vFile.readlines() # open the file as bytes
-        if len(currData) == 0 or not currData[0].startswith(b"head\t"): continue # skip files without ',v' encryption
 
+        with open(currFile,'rb') as vFile: currData = vFile.readlines() # open the file as bytes
+
+        if len(currData) == 0 or not currData[0].startswith(b"head\t"): continue # skip files without ',v' encryption
+        
         for pos in range(len(currData)): # get file revision list + dates
             if currData[pos].startswith(b"date\t"): versionList.append((currData[pos-1],currData[pos]))
 
@@ -29,9 +36,10 @@ def main():
             currVer = revision[0].replace(b".",b"\.") # conpile a new regex
             pattern = re.compile( currVer + b"log\n@(.*?)@\ntext\n@(.*?)@\n\n\n", re.DOTALL)
             result = re.search(pattern, b"".join(currData) + b"\n\n") # get log info + data for each revision
+            
             if len(result.group(2)) == 0:  continue # skip if no data in that revision
-
             if result != None: # skip if no regex search results
+                #encoding = chardet.detect(currData[0])['encoding'] # detect file encoding
                 newFile = currFile[:len(scriptPath)] + "/co" + currFile[len(scriptPath):]
                 newFile = newFile.split('/')
                 newFile[-1] = str(revision[0])[2:-3] + "_" + newFile[-1]
@@ -43,7 +51,10 @@ def main():
 
                 # Log to console
                 logVer, logDate = str(revision[0])[2:-3], str(revision[1]).split("\\t")
-                print(f"Rev: {logVer}; Date: {logDate[1]} {logDate[2]} \nLog: {str(result.group(1))[2:-3]}\nSize: {len(result.group(2))} bytes\n")
+                logMsg = result.group(1).decode(encoding) # decode log messages
+                print(f"Rev: {logVer}; Date: {logDate[1]} {logDate[2]} \nLog: {logMsg}\nFile Size: {len(result.group(2))} bytes\n")
             else: print("No data for", str(revision[0])[2:-3], currFile)
-# run program
-main()
+
+# run program, check arguments first
+if len(sys.argv) > 1: main(sys.argv[1])
+else: main(encodings[1])
