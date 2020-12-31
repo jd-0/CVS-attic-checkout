40cc601734d8ac017f2ad1e0b3a018ee97242f53
jd-0 <44876081+jd-0@users.noreply.github.com>
2020-12-31 12:17:40 -0800

    Update bytePatch.py

--- bytePatch.py
+++ bytePatch.py
@@ -1,24 +1,32 @@
 # Usage:
 # python bytePatch.py rom.sfc 7fc0 "LA LEGENDE DE ZELDA   "
-# python bytePatch.py rom.sfc 7fd9 6
+# python bytePatch.py rom.sfc 0x7fd9 0xaa
+# python bytePatch.py rom.sfc 7fde 0 0
 
 import sys
 
 if len(sys.argv) == 1: print("USAGE: python bytePatch.py romfile (string/int)") ; sys.exit(1)
-# open input file as byte array, get input offset
+
 with open(sys.argv[1],'rb') as f: buff = bytearray(f.read())
 offset = int(sys.argv[2], 16)
 
-if sys.argv[3].isdecimal(): # check if input is string
-    byteValue = int(sys.argv[3], 16)
-    buff[offset] = byteValue
-    print("Patched byte at offset",hex(offset))
+if len(sys.argv) > 4: # Try to parse input as a list of hex
+    print(sys.argv[3:])
+    inputBytes = sys.argv[3:]
+    for i in range(len(inputBytes)): buff[offset+i] = int(inputBytes[i], 16)
+    #print('1')
 
-else: # convert str to hex, write offset
+elif sys.argv[3].startswith('0x'): # if only one byte input, must specify it is hex
+    inputBytes = int(sys.argv[3], 16)
+    buff[offset] = inputBytes
+    #print('2')
 
-    byteValue = [] # convert string to list of hex, then write to buffer
-    for n in sys.argv[3]: byteValue.append(ord(n))
-    for i in range(len(byteValue)): buff[offset+i] = byteValue[i]
-    print("Patched byte(s) at offset",hex(offset))
+else: # otherwise, treat input as string
+    inputBytes = []
+    for n in sys.argv[3]: inputBytes.append(ord(n)) # conv str to list of hex
+    for i in range(len(inputBytes)): buff[offset+i] = inputBytes[i]
+    #print('3')
+
+print(f"Patched {inputBytes}, at offset {hex(offset)}")
 # write data
 with open(sys.argv[1],'wb') as f: f.write(buff)
